2267b00afbbc5d1be4da575a278299eed940c5c5
kokanemanoj1 <manojkokane@iisc.ac.in>
2025-06-14 16:37:56 +0530

    feat: moved OCR to tools

--- src/BACKEND/app/main.py
+++ src/BACKEND/app/main.py
@@ -2,15 +2,14 @@
 
 from pydantic import BaseModel
 import os
-import uuid
-import cv2
-import pytesseract
+from src.TOOLS.OCR import  get_ocr_text
 from fastapi import FastAPI, UploadFile, File, HTTPException
-from fastapi.responses import FileResponse
+
 
 app = FastAPI()
 
 '''
+#Importing the model
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 from peft import PeftModel, PeftConfig
 import torch
@@ -32,11 +31,7 @@ model = model.to(device)
 '''
 
 
-UPLOAD_FOLDER = 'uploads'
-OUTPUT_FOLDER = 'output'
 
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 class Query(BaseModel):
     input_text: str
@@ -47,37 +42,22 @@ def ping():
 
 
 @app.post("/OCR")
-async def upload_image(image: UploadFile = File(...)):
-    try:
-        # Save the uploaded image
-        filename = f"{uuid.uuid4().hex}.jpg"
-        input_path = os.path.join(UPLOAD_FOLDER, filename)
+async def get_ocr(image: UploadFile = File(...)):
+    image=await(image.read())
+    return get_ocr_text(image)
 
-        with open(input_path, "wb") as f:
-            f.write(await image.read())
+@app.post("/calculator")
+def calculate():
+    return {"message": "Hi , Placeholder for calculator"}
 
-        # Load and process the image
-        img = cv2.imread(input_path)
-        if img is None:
-            raise HTTPException(status_code=400, detail="Failed to read image")
 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
+@app.post("/json_formatter")
+def json_format():
+    return {"message": "Hi , Placeholder for json_formatter"}
 
-        custom_config = r'--oem 3 --psm 6'
-        text = pytesseract.image_to_string(thresh, config=custom_config)
-
-        rows = text.strip().split('\n')
-        output_filename = f"{uuid.uuid4().hex}.txt"
-        output_path = os.path.join(OUTPUT_FOLDER, output_filename)
-        with open(output_path, "w") as f:
-            for item in rows:
-                f.write(f"{item}\n")
-
-        return FileResponse(path=output_path, filename=output_filename, media_type='text/plain')
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+@app.post("/translator")
+def translator(text: str):
+    return {"message": f"Hi , Placeholder for translator-{text}"}
 
 #Inference code for model
 '''
